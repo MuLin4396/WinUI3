df70f9d1cf0e49fa585a578b6e181a57d8b743aa
MuLin <ming439611@outlook.com>
2025-01-09 11:37:02 +0800

    5

--- App1/MainWindow.xaml.cs
+++ App1/MainWindow.xaml.cs
@@ -12,45 +12,65 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using App1.View;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Xaml.Media.Animation;
 
 namespace App1
 {
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
+	public sealed partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
 
-            ExtendsContentIntoTitleBar = true;
+			NavigationViewControl.SelectedItem = NavigationViewControl.MenuItems.OfType<ABI.Microsoft.UI.Xaml.Controls.NavigationViewItem>().First();
+			ContentFrame.Navigate(typeof(View.HomePage), null, new EntranceNavigationTransitionInfo());
 
-            SetTitleBar(AppTitleBar);
-        }
+			SystemBackdrop = new MicaBackdrop() { Kind = MicaKind.Base };
 
-        private string GetAppTitleFromSystem()
-        {
-            return Windows.ApplicationModel.Package.Current.DisplayName;
-        }
+			ExtendsContentIntoTitleBar = true;
+			SetTitleBar(AppTitleBar);
+		}
 
-        private void NavigationView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
-        {
-            if (ContentFrame.CanGoBack) ContentFrame.GoBack();
-        }
+		private string GetAppTitleFromSystem()
+		{
+			return Windows.ApplicationModel.Package.Current.DisplayName;
+		}
+
+		private void NavigationView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+		{
+			if (ContentFrame.CanGoBack) ContentFrame.GoBack();
+		}
 
 
-        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            throw new NotImplementedException();
-            // if (args.IsSettingsInvoked)
-            // {
-            //     ContentFrame.Navigate(typeof(SettingsPage), null, args.RecommendedNavigationTransitionInfo);
-            // }
-        }
+		private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+		{
+			if (args.IsSettingsInvoked)
+			{
+				ContentFrame.Navigate(typeof(SettingsPage), null, args.RecommendedNavigationTransitionInfo);
+			}
+			else if (args.InvokedItemContainer != null && (args.InvokedItemContainer.Tag == null))
+			{
+				Type newPage = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+				ContentFrame.Navigate(newPage, null, args.RecommendedNavigationTransitionInfo);
+			}
+		}
 
-        private void ContentFrame_OnNavigated(object sender, NavigationEventArgs e)
-        {
-            throw new NotImplementedException();
-            // NavigationView.IsBackEnabled = ContentFrame.CanGoBack;
-            // if (ContentFrame.SourcePageType==)
-        }
-    }
+		private void ContentFrame_OnNavigated(object sender, NavigationEventArgs e)
+		{
+			// throw new NotImplementedException();
+			NavigationViewControl.IsBackEnabled = ContentFrame.CanGoBack;
+			if (ContentFrame.SourcePageType == typeof(SettingsPage))
+			{
+				NavigationViewControl.SelectedItem = (NavigationViewItem)NavigationViewControl.SettingsItem;
+			}
+			else if (ContentFrame.SourcePageType != null)
+			{
+				NavigationViewControl.SelectedItem = NavigationViewControl.MenuItems.OfType<NavigationViewItem>().First(n => n.Tag.Equals(ContentFrame.SourcePageType.FullName.ToString()));
+			}
+
+			NavigationViewControl.Header = ((NavigationViewItem)NavigationViewControl.SelectedItem)?.Content?.ToString();
+		}
+	}
 }
